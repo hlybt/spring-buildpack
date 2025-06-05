e6e4c9197c3b24a35945eae08e6c9274bb766ea1
hlybt <hulyabt@gmail.com>
2025-06-05 17:54:02 +0300

    Initial commit

---
+++ src/main/java/com/hlybt/buildpack/SpringBuildpackApplication.java
@@ -0,0 +1,13 @@
+package com.hlybt.buildpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuildpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuildpackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hlybt/buildpack/SpringBuildpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlybt.buildpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
